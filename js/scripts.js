abeec28a24d2521c86545c7ed6903d6eec04bf3c
Jaime Jeovanny Cortez Flores <jaimecortez@ufg.edu.sv>
2026-04-08 18:19:58 -0600

    Finalización de práctica

--- js/scripts.js
+++ js/scripts.js
@@ -60,11 +60,52 @@ const height = window.innerHeight;
 const width = window.innerWidth;
 
 console.log("Su pantalla posee "+width+" pixeles de ancho y "+height+" pixeles de alto");
-console.log("window.screen.width:"+window.screen.width+"px","window.screen.height:"+window.screen.height
-+"px");
-
+console.log("window.screen.width:"+window.screen.width+"px","window.screen.height:"+window.screen.height+"px");
 console.log("window.screen.availWidth:"+window.screen.availWidth+"px","window.screen.availHeight:"+window.screen.availHeight+"px");
-
 console.log("document.body.clientWidth:"+document.body.clientWidth+"px","document.body.clientHeight:"+document.body.clientHeight+"px");
 
 
+function calcular(psumando,ssumando,salida){
+
+    //SE CAPTURAN LOS VALORES DEL HTML
+    primerSumando = document.getElementById(psumando);
+    segundoSumando = document.getElementById(ssumando);
+
+    //SE REALIZA EL CÁLCULO
+    let calculo = (parseFloat(primerSumando.value) + parseFloat(segundoSumando.value));
+
+    //SE ASIGNA EL VALOR AL CAMPO DE SALIDA
+    document.getElementById(salida).innerHTML = calculo;
+}
+
+
+
+function crearElemento(url = null){
+
+    if(url !== null){
+        url = url;
+    }else{
+        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_F_109008395_OQDupHMza1V6CNOzrJwWAKlaktT4IsRW.jpg";
+    }
+
+    //SE CREA UN NUEVO ELEMENTO EN EL HTML
+    let objetoimagen = document.createElement("img");
+
+    //SE ASIGNAN PROPIEDADES CON VALORES ESPECÍFICOS
+    objetoimagen.id = "imagencreada";
+    objetoimagen.src = url;
+    objetoimagen.alt = "Icono sobre control de videojuego";
+    objetoimagen.title = "Control de videojuego";
+    objetoimagen.width = 220;
+    objetoimagen.height = 165;
+
+    //SE AGREGA UNA ESCUCHA AL EVENTO CLICK;
+    objetoimagen.addEventListener('click',function(){
+        objetoimagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkU5595AC-bbsoPF-Ik0WzUbVj9rnAbs9lkw&s";
+        objetoimagen.width = 225;
+        objetoimagen.height = 225;
+    });
+
+    //SE "PEGA" O "ADHIERE" AL LIENZO (<div>)
+    document.getElementById('lienzo').appendChild(objetoimagen);
+}
